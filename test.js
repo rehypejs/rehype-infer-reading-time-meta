b2d4a12d21d39867b3af07bf9c457aa36044f4ef
Titus Wormer <tituswormer@gmail.com>
2022-01-05 12:01:58 +0100

    Update dev-dependencies

--- test.js
+++ test.js
@@ -7,38 +7,41 @@ import rehypeInferReadingTimeMeta from './index.js'
 const buf = fs.readFileSync('fixture.html')
 
 test('rehypeInferReadingTimeMeta', async (t) => {
+  let file = await rehype().use(rehypeInferReadingTimeMeta).process(buf)
+
   t.deepEqual(
-    (await rehype().use(rehypeInferReadingTimeMeta).process(buf)).data,
+    file.data,
     {meta: {readingTime: [2.276_744, 3.477_06]}},
     'should estimate reading time'
   )
 
+  file = await rehype()
+    .use(rehypeInferReadingTimeMeta, {mainSelector: 'main'})
+    .process(buf)
+
   t.deepEqual(
-    (
-      await rehype()
-        .use(rehypeInferReadingTimeMeta, {mainSelector: 'main'})
-        .process(buf)
-    ).data,
+    file.data,
     {meta: {readingTime: [2.078_285, 3.173_011]}},
     'should support `mainSelector`'
   )
 
+  file = await rehype().use(rehypeInferReadingTimeMeta, {age: 16}).process(buf)
+
   t.deepEqual(
-    (await rehype().use(rehypeInferReadingTimeMeta, {age: 16}).process(buf))
-      .data,
+    file.data,
     {meta: {readingTime: 2.751_701}},
     'should estimate reading time for one age'
   )
 
+  file = await rehype()
+    .use(rehypeInferReadingTimeMeta)
+    .use(rehypeMeta, {twitter: true})
+    .process(
+      '<h1>Build</h1><p><strong>We provide the building blocks</strong>: from tiny, focussed, modular utilities to plugins that combine them to perform bigger tasks. And much, much more. You can build on unified, mixing and matching building blocks together, to make all kinds of interesting new things.</p>'
+    )
+
   t.equal(
-    String(
-      await rehype()
-        .use(rehypeInferReadingTimeMeta)
-        .use(rehypeMeta, {twitter: true})
-        .process(
-          '<h1>Build</h1><p><strong>We provide the building blocks</strong>: from tiny, focussed, modular utilities to plugins that combine them to perform bigger tasks. And much, much more. You can build on unified, mixing and matching building blocks together, to make all kinds of interesting new things.</p>'
-        )
-    ),
+    String(file),
     '<html><head>\n<meta name="twitter:card" content="summary">\n<meta name="twitter:label1" content="Reading time">\n<meta name="twitter:data1" content="1 minute">\n</head><body><h1>Build</h1><p><strong>We provide the building blocks</strong>: from tiny, focussed, modular utilities to plugins that combine them to perform bigger tasks. And much, much more. You can build on unified, mixing and matching building blocks together, to make all kinds of interesting new things.</p></body></html>',
     'should integrate w/ `rehype-meta`'
   )
