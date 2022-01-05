10510a63a384ecc547b8ea6446343c47e5e69b8a
Titus Wormer <tituswormer@gmail.com>
2022-01-05 12:09:30 +0100

    Refactor

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ import {readingTime} from 'hast-util-reading-time'
 /**
  * Plugin to infer file metadata from the document.
  *
- * @type {import('unified').Plugin<[Options?]|[], Root>}
+ * @type {import('unified').Plugin<[Options?]|Array<void>, Root>}
  */
 export default function rehypeInferReadingTimeMeta(options = {}) {
   const {age = [14, 18], mainSelector} = options
