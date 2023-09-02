535f2c7a827ca1cbd26790589753b5fbfb7a2e82
Titus Wormer <tituswormer@gmail.com>
2023-09-02 11:04:02 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -1,50 +1,46 @@
+import assert from 'node:assert/strict'
 import fs from 'node:fs'
-import test from 'tape'
+import test from 'node:test'
 import {rehype} from 'rehype'
 import rehypeMeta from 'rehype-meta'
 import rehypeInferReadingTimeMeta from './index.js'
 
 const buf = fs.readFileSync('fixture.html')
 
-test('rehypeInferReadingTimeMeta', async (t) => {
-  let file = await rehype().use(rehypeInferReadingTimeMeta).process(buf)
+test('rehypeInferReadingTimeMeta', async function (t) {
+  await t.test('should estimate reading time', async function () {
+    const file = await rehype().use(rehypeInferReadingTimeMeta).process(buf)
 
-  t.deepEqual(
-    file.data,
-    {meta: {readingTime: [2.276_744, 3.477_06]}},
-    'should estimate reading time'
-  )
+    assert.deepEqual(file.data, {meta: {readingTime: [2.276_744, 3.477_06]}})
+  })
 
-  file = await rehype()
-    .use(rehypeInferReadingTimeMeta, {mainSelector: 'main'})
-    .process(buf)
+  await t.test('should support `mainSelector`', async function () {
+    const file = await rehype()
+      .use(rehypeInferReadingTimeMeta, {mainSelector: 'main'})
+      .process(buf)
 
-  t.deepEqual(
-    file.data,
-    {meta: {readingTime: [2.078_285, 3.173_011]}},
-    'should support `mainSelector`'
-  )
+    assert.deepEqual(file.data, {meta: {readingTime: [2.078_285, 3.173_011]}})
+  })
 
-  file = await rehype().use(rehypeInferReadingTimeMeta, {age: 16}).process(buf)
+  await t.test('should estimate reading time for one age', async function () {
+    const file = await rehype()
+      .use(rehypeInferReadingTimeMeta, {age: 16})
+      .process(buf)
 
-  t.deepEqual(
-    file.data,
-    {meta: {readingTime: 2.751_701}},
-    'should estimate reading time for one age'
-  )
+    assert.deepEqual(file.data, {meta: {readingTime: 2.751_701}})
+  })
 
-  file = await rehype()
-    .use(rehypeInferReadingTimeMeta)
-    .use(rehypeMeta, {twitter: true})
-    .process(
-      '<h1>Build</h1><p><strong>We provide the building blocks</strong>: from tiny, focussed, modular utilities to plugins that combine them to perform bigger tasks. And much, much more. You can build on unified, mixing and matching building blocks together, to make all kinds of interesting new things.</p>'
+  await t.test('should integrate w/ `rehype-meta`', async function () {
+    const file = await rehype()
+      .use(rehypeInferReadingTimeMeta)
+      .use(rehypeMeta, {twitter: true})
+      .process(
+        '<h1>Build</h1><p><strong>We provide the building blocks</strong>: from tiny, focussed, modular utilities to plugins that combine them to perform bigger tasks. And much, much more. You can build on unified, mixing and matching building blocks together, to make all kinds of interesting new things.</p>'
+      )
+
+    assert.equal(
+      String(file),
+      '<html><head>\n<meta name="twitter:card" content="summary">\n<meta name="twitter:label1" content="Reading time">\n<meta name="twitter:data1" content="1 minute">\n</head><body><h1>Build</h1><p><strong>We provide the building blocks</strong>: from tiny, focussed, modular utilities to plugins that combine them to perform bigger tasks. And much, much more. You can build on unified, mixing and matching building blocks together, to make all kinds of interesting new things.</p></body></html>'
     )
-
-  t.equal(
-    String(file),
-    '<html><head>\n<meta name="twitter:card" content="summary">\n<meta name="twitter:label1" content="Reading time">\n<meta name="twitter:data1" content="1 minute">\n</head><body><h1>Build</h1><p><strong>We provide the building blocks</strong>: from tiny, focussed, modular utilities to plugins that combine them to perform bigger tasks. And much, much more. You can build on unified, mixing and matching building blocks together, to make all kinds of interesting new things.</p></body></html>',
-    'should integrate w/ `rehype-meta`'
-  )
-
-  t.end()
+  })
 })
